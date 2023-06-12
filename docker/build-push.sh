ab444e950fad411bba1cae60c1dac1745fae26b8
Richard Freeman <rich0@gentoo.org>
2023-06-12 19:16:37 -0400

    Disable api+web until ready to test.
    Signed-off-by: Richard Freeman <rich0@gentoo.org>

--- docker/build-push.sh
+++ docker/build-push.sh
@@ -5,5 +5,5 @@ docker build bacula-base --pull --no-cache --tag ghcr.io/rich0/bacula-base:$BACU
 docker build bacula-dir --pull --no-cache --tag ghcr.io/rich0/bacula-dir:$BACULA_VERSION && docker push ghcr.io/rich0/bacula-dir:$BACULA_VERSION
 docker build bacula-fd --pull --no-cache --tag ghcr.io/rich0/bacula-fd:$BACULA_VERSION && docker push ghcr.io/rich0/bacula-fd:$BACULA_VERSION
 docker build bacula-sd --pull --no-cache --tag ghcr.io/rich0/bacula-sd:$BACULA_VERSION && docker push ghcr.io/rich0/bacula-sd:$BACULA_VERSION
-docker build baculum-api --pull --no-cache --tag ghcr.io/rich0/baculum-api:$BACULA_VERSION && docker push ghcr.io/rich0/baculum-api:$BACULA_VERSION
-docker build baculum-web --pull --no-cache --tag ghcr.io/rich0/baculum-web:$BACULA_VERSION && docker push ghcr.io/rich0/baculum-web:$BACULA_VERSION
+#docker build baculum-api --pull --no-cache --tag ghcr.io/rich0/baculum-api:$BACULA_VERSION && docker push ghcr.io/rich0/baculum-api:$BACULA_VERSION
+#docker build baculum-web --pull --no-cache --tag ghcr.io/rich0/baculum-web:$BACULA_VERSION && docker push ghcr.io/rich0/baculum-web:$BACULA_VERSION
